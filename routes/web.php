f3879f67ed5f28a2d1e094a32a5ebade33491c91
Dawid Jasiński <d.jasinski@programylojalnosciowe.pl>
2019-01-14 17:38:13 +0100

    fix

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,7 @@
 
 Auth::routes();
 
-Route::get('/', 'HomeController@index');
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'HomeController@index')->name('home');
 
 Route::middleware(['auth'])->group(function() {
     Route::middleware(['auth.admin'])->group(function() {
