f7f30855ed995ac3df048c3a923870c0b97e38c3
DESKTOP-DMJK3RB\Jasiek <dante.dawid@gmail.com>
2019-01-17 00:55:23 +0100

    reset password email change

--- app/Notifications/ResetPassword.php
+++ app/Notifications/ResetPassword.php
@@ -24,9 +24,9 @@ class ResetPassword extends Notification
         return (new MailMessage)
             ->from('losowanie.tendaj@gmail.com')
             ->greeting('Witaj,')
-            ->subject('Reset hasła w rodzinnym losowaniu')
-            ->line('Otrzymałeś ten e-mail, ponieważ prosiłeś o wysłanie linku  do zresetowaia hasła w rodzinnym losowaniu, kliknij w poniższy przycisk, aby ustawić nowe hasło')
+            ->subject('Reset hasła w Rodzinnym losowaniu')
+            ->line('Otrzymałeś ten e-mail, ponieważ prosiłeś o wysłanie linku do zresetowania hasła w Rodzinnym losowaniu, kliknij w poniższy przycisk, aby ustawić nowe hasło')
             ->action('Zresetuj hasło', url('password/reset', $this->token))
-            ->line('Jeśli to nie Ty prosiłeś o reset hasła, to znaczy, że ktoś zrobił to za Ciebie podając Twój e-mail, niezwłocznie zgłoś ten fakt administratorowi rodzinnego losowania');
+            ->line('Jeśli to nie Ty prosiłeś o reset hasła, to znaczy, że ktoś zrobił to za Ciebie podając Twój e-mail, niezwłocznie zgłoś ten fakt administratorowi Rodzinnego losowania');
     }
 }
