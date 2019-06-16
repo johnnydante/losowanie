defbf66615c5568c88c011e3333021409010defe
johnnydante <dante.dawid@gmail.com>
2019-06-16 14:00:58 +0200

    fix fix

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -43,25 +43,21 @@ class LoginController extends Controller
     {
         $user->logged++;
         $user->save();
+        $year = date('Y')+1;
         foreach (User::all() as $user) {
             if(\Globals::getDateToDiff($user->birthday) > date('Y-m-d')) {
                 $intDiff = date_diff(date_create(\Globals::getDateToDiff($user->birthday)),date_create(date('Y-m-d')))->days;
             } elseif($user->birthday == null) {
                 $intDiff = 444;
             }else {
-                $intDiff = 365 - date_diff(date_create(\Globals::getDateToDiff($user->birthday)),date_create(date('Y-m-d')))->days;
-            }
-            if($intDiff == 365) {
-                $year = date('Y')+1;
-                if ($year % 4 == 0 && $year % 100 != 0 || $year % 400 == 0) {
-                    $user->daysToBirthday = 365;
+                if($year % 4 == 0 && $year % 100 != 0 || $year % 400 == 0) {
+                    $intDiff = 366 - date_diff(date_create(\Globals::getDateToDiff($user->birthday)),date_create(date('Y-m-d')))->days;
                 } else {
-                    if($intDiff == 1 AND ($year % 4 == 0 && $year % 100 != 0 || $year % 400 == 0)) {
-                        $user->daysToBirthday = 0;
-                    } else {
-                        $user->daysToBirthday = $intDiff;
-                    }
+                    $intDiff = 365 - date_diff(date_create(\Globals::getDateToDiff($user->birthday)),date_create(date('Y-m-d')))->days;
                 }
+            }
+            if(($intDiff == 365 AND !($year % 4 == 0 && $year % 100 != 0 || $year % 400 == 0)) OR ($intDiff == 366 AND ($year % 4 == 0 && $year % 100 != 0 || $year % 400 == 0))) {
+                $user->daysToBirthday = 0;
             } else {
                 $user->daysToBirthday = $intDiff;
             }
